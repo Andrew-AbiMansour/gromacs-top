3f3d1fb49087e2dcdaa5154342a1f64297d7c9aa
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-10-03 14:37:08 -0400

    added shebang

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#! /usr/bin/bash
+
 # Use GROMACS preprocessor to generate a binary (tpr) file from the MD input data
 gmx grompp -f params/md.mdp -p top/topol.top -c struct/methanol.gro -o output/methNVE.tpr -po output/mdout.mdp
 
